b52950b4096ac006efc51334a56bb28296252bc4
jonathonchilds <33766594+jonathonchilds@users.noreply.github.com>
2022-04-28 20:48:04 -0400

    VariablesCS Explorer Mode Working

--- Program.cs
+++ Program.cs
@@ -2,11 +2,42 @@
 
 namespace VariablesCS
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            var numberOfCupsOfCoffee = 3;
+            var fullName = "Jonathon Kieran Childs";
+            var todaysDate = DateTime.Now;
+
+            Console.WriteLine();
+            Console.WriteLine($"{fullName} enjoyed {numberOfCupsOfCoffee} cups of coffee on {todaysDate}");
+
+
+
+
+            Console.Write("What is your name? ");
+            var userName = Console.ReadLine();
+            Console.WriteLine($"Hey, {userName}!");
+            Console.WriteLine();
+
+            Console.Write("Please enter a number: ");
+            var firstNumberAsString = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Please enter another number: ");
+            var secondNumberAsString = Console.ReadLine();
+
+            var firstOperand = double.Parse(firstNumberAsString);
+            var secondOperand = double.Parse(secondNumberAsString);
+
+            var sum = firstOperand + secondOperand;
+            var difference = firstOperand - secondOperand;
+            var product = firstOperand * secondOperand;
+            var quotient = firstOperand / secondOperand;
+            var remainder = firstOperand % secondOperand;
+
+            Console.WriteLine($"Your lil numbers have a sum of {sum}, a difference of {difference}, a product of { product}, a quotient of { quotient}, and a remainder of { remainder}.");
+
+        }
     }
-  }
 }
